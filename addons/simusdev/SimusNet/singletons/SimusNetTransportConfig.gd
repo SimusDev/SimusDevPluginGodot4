98832a3c9c889a382562290f59953a806deb7368
SimusDev <simusdev@mail.ru>
2026-04-01 23:16:58 +0300

    Update SimusNetTransportConfig.gd

--- addons/simusdev/SimusNet/singletons/SimusNetTransportConfig.gd
+++ addons/simusdev/SimusNet/singletons/SimusNetTransportConfig.gd
@@ -2,7 +2,6 @@ class_name SimusNetTransportConfig
 extends Resource
 
 @export var enabled: bool = true
-@export var max_packets_per_batch: int = 12
 @export var compression_enabled: bool = true
 @export var compression_threshold_deflate: int = 512
 @export var compression_threshold_zstd: int = 4096
